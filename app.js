dd116e2dc259419f9c583d61693a739a9796529d
Joseph Tingsanchali <charnwoot@gmail.com>
2014-10-13 13:56:11 -0500

    Added click-able element for upvoting, added incrementUpvotes function for posts

--- app.js
+++ app.js
@@ -15,4 +15,7 @@ angular.module('flapperNews', [])
       $scope.posts.push({title: $scope.title, upvotes: 0});
       $scope.title = '';
     }
+    $scope.incrementUpvotes = function(post){
+      post.upvotes += 1;
+    }
   }]);
