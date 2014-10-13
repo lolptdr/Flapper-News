92f54057abd310348ef365dfb596f94ca7842540
Joseph Tingsanchali <charnwoot@gmail.com>
2014-10-13 15:24:25 -0500

    Added addComment function and forms

--- app.js
+++ app.js
@@ -53,5 +53,15 @@ angular.module('flapperNews', ['ui.router'])
     '$stateParams',
     'posts',
     function($scope, $stateParams, posts){
+      $scope.post = posts.posts[$stateParams.id];
 
+      $scope.addComment = function(){
+        if($scope.body === '') {return;}
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
     }]);
