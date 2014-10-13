6ef7714043dfd83b294d82ed7b2412ae90ad6206
Joseph Tingsanchali <charnwoot@gmail.com>
2014-10-13 13:50:45 -0500

    Added form for getting title, appending new post, and preventing blank input for title

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ angular.module('flapperNews', [])
       {title:'post 5', upvotes: 4}
     ];
     $scope.addPost = function(){
-      $scope.posts.push({title: 'A new post!', upvotes: 0});
+      if($scope.title === ''){ return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
     }
   }]);
