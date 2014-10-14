74151198d961e52c6fdd4d59a729e7b5bd455a2e
Joseph Tingsanchali <charnwoot@gmail.com>
2014-10-13 21:56:53 -0500

    Moved Post upvote method into function; Added Post,Get,Put verbs

--- flapper-news/models/Posts.js
+++ flapper-news/models/Posts.js
@@ -5,6 +5,11 @@ var PostSchema = new mongoose.Schema({
   link: String,
   upvotes: {type: Number, default: 0},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  PostSchema.methods.upvote = function(cb){
+    this.upvotes += 1;
+    this.save(cb);
+  };
 });
+
 
 mongoose.model('Post', PostSchema);
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
@@ -32,3 +31,26 @@ router.post('/posts', function(req, res, next){
   });
 });
 
+router.param('post', function(req, res, next, id){
+  var query = Post.findById(id);
+
+  query.exec(function(err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res){
+  res.json(req.post);
+})
+
+router.put('/posts/:post/upvote', function(req, rest, next){
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
